002df7f70d815dd8bbbece3a614e21e08a2d0e66
永帅 <47472096+Ayu-fe@users.noreply.github.com>
2022-04-12 18:43:30 +0800

    Delete db directory

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-  host: '101.42.250.40',
-  user: 'root',
-  password: '@Wysh1979474549',
-  database: 'website',
-  port: '3306',
-})
